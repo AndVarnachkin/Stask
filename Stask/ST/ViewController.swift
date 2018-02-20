c07acc23c78889efc016c61e631dc27c6ddb7a28
AndVarnachkin <poh@example.com>
2018-02-20 14:32:18 +0300

    add project

---
+++ Stask/ST/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ST
+//
+//  Created by AndVar on 19.02.2018.
+//  Copyright © 2018 AndVar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var Button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
